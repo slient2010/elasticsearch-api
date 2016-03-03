ae188fc603785643152357b4d4bec28a9092c240
slient2010 <slient2010@163.com>
2016-03-03 18:33:57 +0800

    update config

--- mclient/config.py
+++ mclient/config.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # 加密密钥
-SECRET_KEY="H2rqkNJVi9f4Uo4WQ5HIqlydxeNlyTSEz5lU/De9qfI="
-
-#SERVER='127.0.0.1'
-SERVER='m0058.baiduwebgame.com'
-PORT=9911
-
-USER="elasticsearchapi"
-PASSWD="NjS2zQ7sAyyTymz0dS-RYstC_EFkH1sE"
+SECRET_KEY="asdfklasjdfkdsjkfjjiozji"
+#server add
+SERVER='127.0.0.1'
+# server port
+PORT=9999
+# user
+USER="admin"
+# password
+PASSWD = "abcdefghijklmnopqrstuvwxyz"
--- mserver/config.py
+++ mserver/config.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-ESEVER="10.96.33.38"
+#Elasticsearch Server and port
+ESEVER="127.0.0.1"
 EPORT=9200
-# 加密密钥串
-SECRET_KEY="H2rqkNJVi9f4Uo4WQ5HIqlydxeNlyTSEz5lU/De9qfI="
-# 使用工具
-AUTO_PLATFORM = "swall"
-# 监听端口
-PORT=9911
-# 认证用户
-USER = "elasticsearchapi"
-# 认证用户密码
-PASSWD = "NjS2zQ7sAyyTymz0dS-RYstC_EFkH1sE"
+# secret key
+SECRET_KEY="asdfklasjdfkdsjkfjjiozji"
+# automatic tools, salt, ansible, puppet etc.
+AUTO_PLATFORM = "salt"
+# listen port
+PORT=9999
+# user
+USER = "admin"
+# password
+PASSWD = "abcdefghijklmnopqrstuvwxyz"
